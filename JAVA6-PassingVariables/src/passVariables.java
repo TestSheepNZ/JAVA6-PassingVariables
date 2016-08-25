6bf6853c72e7c6f7399656f32da263c718a285ab
TestSheepNZ <mike_talks@hotmail.com>
2016-08-25 21:16:12 +1200

    Link

--- JAVA6-PassingVariables/src/passVariables.java
+++ JAVA6-PassingVariables/src/passVariables.java
@@ -1,3 +1,14 @@
+/*
+ * This program is part of my learning Java series
+ * This one looks at passing variables to a method
+ * 
+ * Find the blog here,
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-6-avoiding-tangled-code-with.html
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
 
 public class passVariables {
 
